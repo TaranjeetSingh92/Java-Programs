1d03119b4a16f4b7e3a53f60dc78363d2a4d9622
TaranjeetSingh92 <ertaranjeetsingh1992@gmail.com>
2024-04-01 09:48:19 +0530

    Instantitation, intance vs local variables

--- OOP/Program1.java
+++ OOP/Program1.java
@@ -1,7 +1,25 @@
-class Program1
+package OOP;
+class Demo
 {
-    public static void main(String args[])
+    int avg;    //instance variable heap
+    int age;    //instance variable heap
+    void disp() //stack
     {
-        
+        int num1=100; //local variable stack
+        int num2=150;//local variable stack
+        //avg=50;
+        //age=33;
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(avg);
+        System.out.println(age);
     }
+}
+public class Program1
+{
+    public static void main(String args[]) //JVM will start from here //in stack area, stack frame created
+    {
+        Demo obj=new Demo(); 
+        obj.disp();//stack frame created for demo()
+    }//stack frame deleted , garbage collector will delete all refrences in heap
 }
